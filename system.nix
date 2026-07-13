ea6abec442e4a61c0ff7ca8ddea8e2c4ff9cc144
Aether <dylanmather2507@outlook.com>
2026-07-13 20:02:15 +0100

    Add initial NixOS configuration in system.nix

---
+++ system.nix
@@ -0,0 +1,74 @@
+
+
+#                                    ██████   █████  ███             
+#                                   ▒▒██████ ▒▒███  ▒▒▒              
+#                                    ▒███▒███ ▒███  ████  █████ █████
+#                                    ▒███▒▒███▒███ ▒▒███ ▒▒███ ▒▒███ 
+#                                    ▒███ ▒▒██████  ▒███  ▒▒▒█████▒  
+#                                    ▒███  ▒▒█████  ▒███   ███▒▒▒███ 
+#                                    █████  ▒▒█████ █████ █████ █████
+#                                   ▒▒▒▒▒    ▒▒▒▒▒ ▒▒▒▒▒ ▒▒▒▒▒ ▒▒▒▒▒ 
+#                                 
+#                                                          █████████                      █████                            
+#                                                         ███▒▒▒▒▒███                    ▒▒███                             
+#                                                        ▒███    ▒▒▒  █████ ████  █████  ███████    ██████  █████████████  
+#                                                        ▒▒█████████ ▒▒███ ▒███  ███▒▒  ▒▒▒███▒    ███▒▒███▒▒███▒▒███▒▒███ 
+#                                                         ▒▒▒▒▒▒▒▒███ ▒███ ▒███ ▒▒█████   ▒███    ▒███████  ▒███ ▒███ ▒███ 
+#                                                         ███    ▒███ ▒███ ▒███  ▒▒▒▒███  ▒███ ███▒███▒▒▒   ▒███ ▒███ ▒███ 
+#                                                        ▒▒█████████  ▒▒███████  ██████   ▒▒█████ ▒▒██████  █████▒███ █████
+#                                                         ▒▒▒▒▒▒▒▒▒    ▒▒▒▒▒███ ▒▒▒▒▒▒     ▒▒▒▒▒   ▒▒▒▒▒▒  ▒▒▒▒▒ ▒▒▒ ▒▒▒▒▒ 
+#                                                                      ███ ▒███                                            
+#                                                                     ▒▒██████                                             
+#                                                                      ▒▒▒▒▒▒
+
+
+{ config, pkgs, ... }:
+
+{
+  hardware.enableRedistributableFirmware = true;
+
+hardware.bluetooth = {
+  enable = true;
+  powerOnBoot = true;
+};
+
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/London";
+
+  i18n.defaultLocale = "en_GB.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_GB.UTF-8";
+    LC_IDENTIFICATION = "en_GB.UTF-8";
+    LC_MEASUREMENT = "en_GB.UTF-8";
+    LC_MONETARY = "en_GB.UTF-8";
+    LC_NAME = "en_GB.UTF-8";
+    LC_NUMERIC = "en_GB.UTF-8";
+    LC_PAPER = "en_GB.UTF-8";
+    LC_TELEPHONE = "en_GB.UTF-8";
+    LC_TIME = "en_GB.UTF-8";
+  };
+
+  console.keyMap = "uk";
+
+  services.xserver.xkb = {
+    layout = "gb";
+    variant = "";
+  };
+
+  users.users.aether = {
+    isNormalUser = true;
+    description = "aether";
+    extraGroups = [
+      "networkmanager"
+      "wheel"
+      "audio"
+      "video"
+      "input"
+    ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+}
